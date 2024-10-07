e57ab0ca38143b1ca3774778d255ad010a9a3082
Conrad Kay <conradkaydev@gmail.com>
2024-10-06 22:01:01 -0400

    check for spaces in board

--- src/lib/eval/eval.ts
+++ src/lib/eval/eval.ts
@@ -3,6 +3,8 @@ import { convertCardsToNumbers, DECK, evaluate } from './strength'
 export const boardToInts = (board: string) => {
   const boardInts: number[] = []
 
+  board = board.replaceAll(' ', '')
+
   for (let i = 0; i < board.length; i += 2) {
     boardInts.push(DECK[(board[i] + board[i + 1]).toLowerCase()])
   }
